ff4cdc9d89f4b296484ba6052a7cd41dc6f61b07
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-12-05 16:19:10 +0800

    type fixing

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,9 @@ export default {
   schema: './src/lib/schema.ts',
   driver: 'pg',
   dbCredentials: {
-    host: process.env.RAILWAY_TCP_PROXY_DOMAIN,
-    port: process.env.RAILWAY_TCP_PROXY_PORT,
-    database: process.env.POSTGRES_DB,
+    host: process.env.RAILWAY_TCP_PROXY_DOMAIN ?? '',
+    port: +(process.env.RAILWAY_TCP_PROXY_PORT ?? '0'),
+    database: process.env.POSTGRES_DB ?? '',
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     ssl: false,
